a6b2d9e45d1fbfa43d6feaa01362424bee237341
Kyryl <kyryl.zp@gmail.com>
2022-09-07 20:11:58 +0300

    create js-world

--- index.js
+++ index.js
@@ -6,26 +6,65 @@
    */
 
 // ======== OBJECTS DEFINITIONS ========
-// Define your objects here
 
+const dog = {
+    species: 'dog',
+    name: 'Toby',
+    gender: 'male',
+    legs: 4,
+    hands: 0,
+    saying: 'woof-woof!'
+};
+
+const cat = {
+    species: 'cat',
+    name: 'Tom',
+    gender: 'male',
+    legs: 4,
+    hands: 0,
+    saying: 'may-may!'
+};
+
+const woman = {
+    species: 'human',
+    name: 'Inna',
+    gender: 'female',
+    legs: 2,
+    hands: 2,
+    saying: 'Hello world!'
+};
+
+const man = {
+    species: 'human',
+    name: 'Kyryl',
+    gender: 'male',
+    legs: 2,
+    hands: 2,
+    saying: 'Hello world!'
+};
 
 // ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
 
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
+const inhabitants = [
+    dog,
+    cat,
+    woman,
+    man
+];
 
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
+const props = [
+    'species',
+    'name',
+    'gender',
+    'legs',
+    'hands',
+    'saying'
+];
 
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+inhabitants.forEach(elem => {
+    print(
+        props.filter(prop => elem[prop] !== 0 ).map(prop => elem[prop] ).join("; ")
+    );
+});
 
 
